2c7875d71aaef1164d87303e96a4399fdd325e77
Giorgio Ziu <giorgio.ziu.alo@gmail.com>
2022-11-02 12:59:09 +0100

    Cleaned structure and updated comment-removing execution flow

--- lib.ps1
+++ lib.ps1
@@ -1,79 +1,29 @@
 # COSTANTS
 # __________________________________________________
 # ILC: InLine Comment
-# MLC: MultiLine Comment
 $RegexILC = '(?smx)<!--.*?-->'
-$RegexMLCOpening = '^<!--(?!.*-->).*$'
-$RegexMLCClosing = '^(?!<!--.*).*-->$'
-$RegexHTML = '(?<tag><\/?[a-zA-Z]+\s*(?<attr>\s*[a-zA-Z]+=".*"\s*)*?\s*>)'
+$RegexIsCode = '(?<tag><\/?[a-zA-Z]+\s*(?<attr>\s*[a-zA-Z]+=".*"\s*)*?\s*>)'
 
 # VARIABLES
 # __________________________________________________
-$Global:MLCFlag = $false
-$Global:MLCLines = ""
-$Global:CommentCounter = 0
 
-function Print-Fixed-Comment {
-  param([string]$Line, [string]$Fixed)
+function Print-Comment {
+  param([string]$Comment, [string]$FileName)
 
-  Write-Host "-----------------------------------------------------------------"
-  Write-Host "Comment number: $Global:CommentCounter"
-  Write-Host "Original:`n`n$Line`n"
-  Write-Host "Fixed:`n`n$Fixed`n"
-  Write-Host "-----------------------------------------------------------------"
-}
-
-function Check-Line {
-  param([string]$Line)
-
-  # Write-Host $Line
-  # Write-Host "__________________________________________________"
-  # Write-Host (Select-String $RegexMLCOpening -InputObject $Line -AllMatches).Matches.Count
-  # Write-Host (Select-String $RegexMLCClosing -InputObject $Line -AllMatches).Matches.Count
-  # Write-Host (Select-String $RegexILC -InputObject $Line -AllMatches).Matches.Count
-  # Check for multiple inline comments
-  if ((Select-String $RegexMLCOpening -InputObject $Line -AllMatches).Matches.Count +
-    (Select-String $RegexMLCClosing -InputObject $Line -AllMatches).Matches.Count +
-    (Select-String $RegexILC -InputObject $Line -AllMatches).Matches.Count -ge 2) {
-      return 4
-    }
-
-  if ($Line -match $RegexILC) {
-    return 1
-  }
-
-  if ($Line -match $RegexMLCOpening) {
-    return 2
-  }
-
-  if ($Line -match $RegexMLCClosing) {
-    return 3
-  }
-
-  return 0
+  Write-Host "File: $FileName"
+  Write-Host "--------------------------------------------------"
+  Write-Host $Comment
+  Write-Host
+  Write-Host
 }
 
 function Check-Comment {
   param([string]$Comment)
 
-  if ($Comment -match $RegexHTML) {
+  if ($Comment -match $RegexIsCode) {
     return $true
   }
   return $false
-}
-
-function Clean-Comment {
-  param([string]$Comment)
-
-  $InfoMatch = Select-String -inputObject $Comment -pattern $RegexILC -allMatches
-  $Cleaned = ""
-
-  Foreach ($Match in $InfoMatch.matches) {
-    $Cleaned += "$Match`n"
-
-  }
-
-  return $Cleaned
 }
 
 function Delete-Comment {
@@ -84,73 +34,28 @@ function Delete-Comment {
   Set-Content -path $FilePath -value $Lines
 }
 
-function Manage-Line {
-  param([string]$Line, [int]$LineStatus)
+function Remove-Comments {
+  param([string]$TargetDirectory, [string]$TargetExtension)
 
-  # Based on the line status we take an action
-  Switch ($LineStatus) {
+  # Get all files and loop through them
+  $Files = Get-ChildItem -path $TargetDirectory -recurse -file -filter "*.$TargetExtension"
+  ForEach ($File in $Files) {
 
-    # No comment, or uncommented line, or inside a multiline comment
-    0 {
-      # Check if we are currently inside a multiline comment
-      if ($Global:MLCFlag) {
-        $Global:MLCLines += "$Line`n"
+    # Get all lines as a string
+    $Lines = Get-Content -path $File.FullName -raw
+
+    # Get everything that is a comment
+    $Comments = (Select-String -Pattern $RegexILC -InputObject $Lines -AllMatches).Matches
+
+    ForEach ($Comment in $Comments) {
+
+      # Check if it's code or helpful info
+      if (Check-Comment $Comment) {
+        Print-Comment $Comment $File.Name
       }
-
-      break;
     }
 
-    # Inline comment
-    1 {
-
-      # Check if the comment is code or useful info
-      if (Check-Comment $Line) {
-        $Global:CommentCounter += 1
-        Print-Fixed-Comment $Line (Clean-Comment $Line)
-      }
-      break;
-    }
-
-    # Opening comment
-    2 {
-      $Global:MLCFlag = $true
-      $Global:MLCLines = "$Line`n"
-
-      break;
-    }
-
-    # Closing comment
-    3 {
-      $Global:MLCFlag = $false
-      $Global:MLCLines += $Line
-
-      if (Check-Comment $Global:MLCLines) {
-        $Global:CommentCounter += 1
-        Print-Fixed-Comment $Global:MLCLines (Clean-Comment $Global:MLCLines)
-      }
-
-      break;
-    }
-
-    # Multiple inline comments
-    4 {
-      $Comments = (Select-String $RegexMLCClosing -InputObject $Line).Matches
-      $Comments += (Select-String $RegexILC -InputObject $Line -AllMatches).Matches
-      $Comments += (Select-String $RegexMLCOpening -InputObject $Line).Matches
-
-      Foreach ($Comment in $Comments) {
-        # Write-Host $Comment
-        if (Check-Comment $Comment) {
-          Manage-Line $Comment (Check-Line $Comment)
-        }
-      }
-
-      break;
-    }
-
-    default {
-      Write-Host "Something unexpected happened."
-      break;
-    }
   }
+
+
 }
--- main.ps1
+++ main.ps1
@@ -6,7 +6,8 @@ Clear-Host
 # COSTANTS
 # __________________________________________________
 # $TargetDir = "C:\xampp\htdocs\test\contrib\themes\primaton-theme\client"
-$TargetDir = ".\test"
+$TargetDirectory = ".\test"
+$TargetExtension = "html"
 
 # VARIABLES
 # __________________________________________________
@@ -14,27 +15,4 @@ $TargetDir = ".\test"
 # MAIN
 # __________________________________________________
 
-# Get all files and loop through them
-$Files = Get-ChildItem -path $TargetDir -recurse -file -filter *.html
-ForEach ($File in $Files) {
-  # Get all lines and loop through them
-  $Lines = Get-Content -path $File.FullName -raw
-
-  $abba = (Select-String -Pattern $RegexILC -InputObject $Lines -AllMatches).Matches
-
-  ForEach ($gino in $abba) {
-    if (Check-Comment $gino) {
-      Write-Host "-----------------------------------------------------------------"
-      Write-Host $gino
-    }
-  }
-
-  #
-  # ForEach ($Line in $Lines) {
-  #
-  #   # Check if the line has an inline, opening or closing comment
-  #   # $LineStatus = Check-Line $Line
-  #
-  #   Manage-Line $Line $LineStatus
-  # }
-}
+Remove-Comments $TargetDirectory $TargetExtension
